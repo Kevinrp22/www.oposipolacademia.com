0b1fa9bf17f3f5e29b4988fa9ad5be9a292e9e34
mat130517 <matiassalave@gmail.com>
2020-06-11 00:05:39 +0200

    Condicional "sideEstado" cambiado. 2 espacios en TABS

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
-  import { onMount } from "svelte";
-  import { Router, Link, Route } from "svelte-routing";
+  import {
+    onMount
+  } from "svelte";
+  import {
+    Router,
+    Link,
+    Route
+  } from "svelte-routing";
   import Contacto from "./routes/Contacto.svelte";
   import Cursos from "./routes/Cursos.svelte";
   import Home from "./routes/Home.svelte";
@@ -26,14 +32,61 @@
     sideEstado = false;
   };
 
-  $: {
-    if (sideEstado == true) {
-      s_body.overflow = "hidden";
-    } else {
-      s_body.overflow = "initial";
-    }
-  }
+  $: sideEstado ? s_body.overflow = "hidden": s_body.overflow = "initial" 
+  
 </script>
+
+<Router {url}>
+  <header>
+    <nav>
+      <div class="menu">
+        <Link to="/">Home</Link>
+        <Link to="cursos">Cursos</Link>
+        <Link to="contacto">Contacto</Link>
+      </div>
+      <div class="c-logo">
+        <Link to="/">
+        <img class="c-logo__img" src="assets/logo-negro.png" alt="logo-oposipol" />
+        </Link>
+      </div>
+
+      <div class="menu">
+        <Link to="noticias">Noticias</Link>
+        <Link to="remius">
+        Remius
+        <i class="fas fa-sign-in-alt" />
+        </Link>
+
+      </div>
+
+      <div id="barra" on:click={openSideBar}>
+        <i class="fas fa-bars" />
+      </div>
+
+    </nav>
+    <div class="sidebar" class:active={sideEstado==true ? 'active' : '' }>
+      <div class="sidebar_btn" on:click={closeSideBar}>
+        <i class="fas fa-arrow-left" />
+      </div>
+      <div class="sidebar_nav">
+        <Link to="/">Home</Link>
+        <Link to="cursos" class="sidebar_nav__items">Cursos</Link>
+        <Link to="contacto" class="sidebar_nav__items">Contacto</Link>
+        <Link to="noticias" class="sidebar_nav__items">Noticias</Link>
+        <Link to="remius" class="sidebar_nav__items">
+        Remius
+        <i class="fas fa-sign-in-alt" />
+        </Link>
+      </div>
+    </div>
+  </header>
+  <Route path="contacto" component={Contacto} />
+  <Route path="cursos" component={Cursos} />
+  <Route path="/">
+    <Home />
+  </Route>
+</Router>
+
 
 <style>
   header {
@@ -42,6 +95,7 @@
     background-color: black;
     z-index: 1111;
   }
+
   a {
     display: block;
     text-decoration: none;
@@ -59,7 +113,7 @@
     display: block;
     width: 230px;
   }
-  
+
 
   .menu {
     border-radius: 3px;
@@ -72,6 +126,7 @@
     color: white;
     cursor: pointer;
   }
+
   .sidebar {
     position: absolute;
     top: 0;
@@ -84,9 +139,11 @@
     transition: var(--transition);
     transform: translate3d(-100%, 0, 0);
   }
+
   .sidebar_btn {
     background-color: #353535;
   }
+
   .sidebar_btn i {
     cursor: pointer;
     font-size: 20px;
@@ -94,9 +151,11 @@
     color: rgb(53, 53, 53);
     background-color: #2697d8;
   }
+
   .sidebar_btn i:hover {
     background-color: #2697d8a9;
   }
+
   .sidebar_nav__items {
     display: flex;
     color: #ffffff;
@@ -108,18 +167,21 @@
     margin: 0.9px 0px;
     transition: var(--transition);
   }
+
   .sidebar_nav__items i {
     font-size: 0.9rem;
     color: white;
     margin-left: 10px;
     transition: var(--transition);
   }
+
   .sidebar_nav__items:hover {
     color: #2697d8;
 
     background-color: #4d4d4d;
   }
-  .sidebar_nav__items:hover > i {
+
+  .sidebar_nav__items:hover>i {
     color: #2697d8;
   }
 
@@ -131,80 +193,23 @@
     #barra i {
       display: block;
     }
+
     .menu {
       display: none;
     }
+
     nav {
       justify-content: space-between;
     }
+
     .c-logo__img {
       width: 200px;
     }
   }
+
   @media only screen and (min-width: 1000px) {
     .menu-desplegable {
       display: none !important;
     }
   }
 </style>
-
-<Router {url}>
-  <header>
-    <nav>
-
-      <div class="menu">
-
-        <Link to="/">Home</Link>
-
-        <Link to="cursos">Cursos</Link>
-
-        <Link to="contacto">Contacto</Link>
-
-      </div>
-
-      <div class="c-logo">
-        <Link to="/">
-          <img
-            class="c-logo__img"
-            src="assets/logo-negro.png"
-            alt="logo-oposipol" />
-        </Link>
-
-      </div>
-
-      <div class="menu">
-        <Link to="noticias">Noticias</Link>
-        <Link to="remius">
-          Remius
-          <i class="fas fa-sign-in-alt" />
-        </Link>
-
-      </div>
-
-      <div id="barra" on:click={openSideBar}>
-        <i class="fas fa-bars" />
-      </div>
-
-    </nav>
-    <div class="sidebar" class:active={sideEstado == true ? 'active' : ''}>
-      <div class="sidebar_btn" on:click={closeSideBar}>
-        <i class="fas fa-arrow-left" />
-      </div>
-      <div class="sidebar_nav">
-        <Link to="/">Home</Link>
-        <Link to="cursos" class="sidebar_nav__items">Cursos</Link>
-        <Link to="contacto" class="sidebar_nav__items">Contacto</Link>
-        <Link to="noticias" class="sidebar_nav__items">Noticias</Link>
-        <Link to="remius" class="sidebar_nav__items">
-          Remius
-          <i class="fas fa-sign-in-alt" />
-        </Link>
-      </div>
-    </div>
-  </header>
-  <Route path="contacto" component={Contacto} />
-  <Route path="cursos" component={Cursos} />
-  <Route path="/">
-    <Home />
-  </Route>
-</Router>
